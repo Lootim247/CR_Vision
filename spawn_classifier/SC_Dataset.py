ce6a32bdc0c3e01566afc7a106ff8b60d9b04cad
Tim Panilaitis <tpanilai@gmail.com>
2025-11-24 15:20:05 -0500

    Cleaned up dataset and prepped train_classifier for k-fold validation

--- spawn_classifier/SC_Dataset.py
+++ spawn_classifier/SC_Dataset.py
@@ -11,12 +11,19 @@ import numpy as np
 import sys
 import json
 
-# Inherets Dataset and overwrites len and getitem
 class SC_Dataset(Dataset):
-    def __init__(self, title="main", data_path='data/', transform=None):
-        self.title = title
+    """
+    INPUTS:
+        data_set:   Expected to be a numpy array of shape [SIZE, 3] with sub-
+                    array of property [VIDEO FRAME LABEL]
+        data_path:  Should be the relative path to the data directory. Data is
+                    expected to have a subfolder named "frame_arrays"
+        transform:  A composition of pytorch transform function. If NONE auto
+                    converts to tensor. 
+    """
+    def __init__(self, data_set, data_path='data/', transform=None):
         self.data_path = data_path
-        self.data = np.loadtxt(f'{data_path}{title}.txt')
+        self.data = np.array(data_set)
         self.video_cache = None
 
         if transform is None:
@@ -24,14 +31,14 @@ class SC_Dataset(Dataset):
         else:
             self.transform = transform
 
-    # must return the length of the data as int
     def __len__(self):
+        """
+        OUTPUTS:
+            length: The number of datapoints in the provided dataset.
+        """
         return self.data.shape[0]
 
-    # must return the image and class in (image_tensor, label_tensor) tuple
-    def __getitem__(self, index):
-        video, frame, label = self.data[index].astype(np.int32)
-
+    def __get_item_helper(self, video, frame, label):
         json_path = f'{self.data_path}frame_arrays/{video}_meta.json'
         with open(json_path, "r", encoding="utf-8") as f:
             self.json = json.load(f)
@@ -45,12 +52,33 @@ class SC_Dataset(Dataset):
             self.json["width"], 
             3))
 
-        frame_data = frames[frame]
-
-        # print(f'Frame_data shape: {frame_data.shape}')
-        # print(f'type of frame_data: {type(frame_data)}')
-
+        frame_data = np.array(frames[frame])
         if frame_data is None:
             raise ValueError(f'Video:{video}[{frame}] is None')
+        
+        return [self.transform(frame_data.copy()), torch.tensor(label, dtype=torch.long)]
 
-        return (self.transform(frame_data.copy()), torch.tensor(label, dtype=torch.long))
+    def __getitem__(self, index):
+        """
+        INPUTS:
+            index:  The index or array of index of the desired item(s) in the 
+                    provided dataset.
+        OUTPUTS:
+            (frame tensor, :    The respective frame(s) refered to at the given 
+                                dataset index(es).
+            label tensor ) :    The respective label(s) at the given dataset
+                                index(es).
+        """
+        
+        if np.isscalar(index):
+            video, frame, label = self.data[index].astype(np.int32)
+            return self.__get_item_helper(video, frame, label)
+        else:
+            if not isinstance(index, np.ndarray):
+                index = np.array(index)
+            
+            data = []
+            for j in range(index.shape[0]):
+                video, frame, label = self.data[index[j]].astype(np.int32)
+                data.append(self.__get_item_helper(video, frame, label))
+            return np.array(data)
--- train_classifier.py
+++ train_classifier.py
@@ -2,47 +2,73 @@ from spawn_classifier import SC_Dataset, SC_Model
 from torchvision import transforms
 from torch.utils.data import DataLoader
 import torch.nn as nn
+import torch
 import torch.optim as optim
 import numpy as np
-from PIL import Image
+from sklearn.model_selection import KFold
+import sys
 
 
-print("The Start")
+# Initialize our Transpose
 t = transforms.Compose([
     transforms.Resize((224,224)),
     transforms.ToTensor()
 ])
 
-dataset = SC_Dataset.SC_Dataset(f'main', transform=None)
-data_loader = DataLoader(dataset, batch_size=32, shuffle=True)
-print("Initialized Data Loaders")
+# initialize our dataset and splitters
+data = np.loadtxt("data/main.txt")
+kf = KFold(n_splits=5, shuffle=True, random_state=42)
 
-model = SC_Model.SimpleCNN()
-num_epochs = 2
-print("Initialized Model")
+# train the data on a certain index. Then evaluate with the validation idx
+for fold, (train_idx, val_idx) in enumerate(kf.split(data)):
+    train = data[train_idx]
+    test = data[val_idx]
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-model.train()
-print("Initialized Training")
+    # initialize both loaders
+    train = SC_Dataset.SC_Dataset(train, transform=None)
+    test = SC_Dataset.SC_Dataset(test, transform=None)
+    train_loader = DataLoader(train, batch_size=64, shuffle=True)
+    val_loader   = DataLoader(test, batch_size=64)
 
-for epoch in range(num_epochs):
-    running_loss = 0.0
-    for i, data in enumerate(data_loader, 0):
-        inputs, labels = data
-        inputs = inputs.float()
+    # initialize simple model
+    model = SC_Model.SimpleCNN()
+    num_epochs = 2
+    
+    # train the model 
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    model.train()
+    for epoch in range(num_epochs):
+        running_loss = 0.0
+        for i, data in enumerate(train_loader, 0):
+            inputs, labels = data
+            inputs = inputs.float()
 
-        # zero the parameter gradients
-        optimizer.zero_grad()
+            # zero the parameter gradients
+            optimizer.zero_grad()
 
-        # forward + backward + optimize
-        outputs = model(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+            # forward + backward + optimize
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-        # print statistics
-        running_loss += loss.item()
-        if i % 100 == 99:    # print every 2000 mini-batches
-            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 100:.3f}')
-            running_loss = 0.0
+            # print statistics
+            running_loss += loss.item()
+            if i % 100 == 99:    # print every 100 mini-batches
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 100:.3f}')
+                running_loss = 0.0
+    
+    # evaluate the model 
+    # model.eval()
+    # running_loss = 0.0
+    # with torch.no_grad:
+    #     for images, labels in val_loader:
+    #         outputs = model(data)
+    #         loss = criterion(outputs, labels)
+    #         running_loss += loss.item() * inputs.size(0)
+    #     val_loss = running_loss / len(val_loader.dataset)
+    #     val_losses.append(val_loss)
+        
+sys.exit(1)
+
